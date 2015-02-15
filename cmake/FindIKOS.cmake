c946aa4d567e42a2b30d5e9a217a480f90ddc040
Arie Gurfinkel <arie@cmu.edu>
2015-02-15 09:57:34 -0500

    Updated FindIKOS script

--- cmake/FindIKOS.cmake
+++ cmake/FindIKOS.cmake
@@ -1,9 +1,11 @@
 if (NOT IKOS_FOUND)
-  find_path(IKOS_INCLUDE_DIR NAMES Cfg.hpp PATHS ${IKOS_ROOT}/include/ikos_cfg NO_DEFAULT_PATH)
-  get_filename_component(IKOS_INCLUDE_DIR ${IKOS_INCLUDE_DIR} DIRECTORY)
+  set (IKOS_ROOT "" CACHE PATH "Search path for ikos-core")
+  message (STATUS "Ikos root: ${IKOS_ROOT}")
+  find_path(IKOS_INCLUDE_DIR NAMES ikos_cfg/Cfg.hpp
+    PATHS ${IKOS_ROOT}/include  NO_DEFAULT_PATH)
   find_library(IKOS_DBM_LIB NAMES dbm PATHS ${IKOS_ROOT}/lib NO_DEFAULT_PATH)
-
   include (FindPackageHandleStandardArgs)
+  
   find_package_handle_standard_args(IKOS REQUIRED_VARS IKOS_INCLUDE_DIR IKOS_DBM_LIB) 
-  mark_as_advanced(IKOS_SEARCH_PATH IKOS_INCLUDE_DIR IKOS_DBM_LIB)
+  mark_as_advanced(IKOS_ROOT IKOS_INCLUDE_DIR IKOS_DBM_LIB)
 endif()
