7508b4d9b00e447dfbb5e61bc1771c53c938ae6b
Arie Gurfinkel <arie@cmu.edu>
2015-02-27 19:49:47 -0500

    set ikos version number

--- cmake/FindIKOS.cmake
+++ cmake/FindIKOS.cmake
@@ -8,4 +8,11 @@ if (NOT IKOS_FOUND)
   
   find_package_handle_standard_args(IKOS REQUIRED_VARS IKOS_INCLUDE_DIR IKOS_DBM_LIB) 
   mark_as_advanced(IKOS_ROOT IKOS_INCLUDE_DIR IKOS_DBM_LIB)
+  # start from 1 to make cmakedefine happy
+  set (IKOS_MAJOR_VERSION 1)
+  if (EXISTS ${IKOS_ROOT}/lib/dbm/dbm.cpp)
+    set (IKOS_MINOR_VERSION 1)
+  else()
+    set (IKOS_MINOR_VERSION 2)
+  endif()
 endif()
