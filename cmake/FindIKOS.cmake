f4a9ad431bdda3a02f9909fd680880591e71382f
Arie Gurfinkel <arie@cmu.edu>
2015-02-27 22:59:14 -0500

    unifying cmake

--- cmake/FindIKOS.cmake
+++ cmake/FindIKOS.cmake
@@ -4,10 +4,12 @@ if (NOT IKOS_FOUND)
   find_path(IKOS_INCLUDE_DIR NAMES ikos/cfg/Cfg.hpp
     PATHS ${IKOS_ROOT}/include  NO_DEFAULT_PATH)
   find_library(IKOS_DBM_LIB NAMES dbm PATHS ${IKOS_ROOT}/lib NO_DEFAULT_PATH)
-  include (FindPackageHandleStandardArgs)
+  message (STATUS "IKOS_DBM_LIB: ${IKOS_DBM_LIB}")
   
+  include (FindPackageHandleStandardArgs)
   find_package_handle_standard_args(IKOS REQUIRED_VARS IKOS_INCLUDE_DIR) 
   mark_as_advanced(IKOS_ROOT IKOS_INCLUDE_DIR IKOS_DBM_LIB)
+  message (STATUS "Found Ikos at ${IKOS_INCLUDE_DIR}")
   # start from 1 to make cmakedefine happy
   set (IKOS_MAJOR_VERSION 1)
   if (IKOS_DBM_LIB)
