4f71f11308e6ffea901eea4ee1d6bcab89507b63
jorge <navasjorgea@gmail.com>
2015-12-16 17:48:05 -0800

    Added more tests

---
+++ tests/test-arr-10.c
@@ -0,0 +1,13 @@
+int a[10];
+
+int main (){
+  int *p = &a[0];
+  while (1) {
+    *p = 5;
+    p++;
+    if (p >= 10)
+      break;
+  }
+  int *q = &a[0];
+  return *(q+4);
+}
---
+++ tests/test-global-1.c
@@ -0,0 +1,29 @@
+extern int __VERIFIER_NONDET();
+extern void __VERIFIER_error() __attribute__((noreturn));
+
+int e=0;
+int s=2;  
+
+// we should get e=[0,2] and s=[2,5] with thresholds or without applying widening
+// we should get e=[0,+oo] and s=[2,5] otherwise
+
+int main () {
+
+  while (__VERIFIER_NONDET()) {
+    if (s == 2){
+      if (e ==0) e=1;
+      s = 3;
+    }
+    else if (s == 3){
+      if (e ==1) e=2;
+      s=4;
+    }
+    else if (s == 4){
+      if (e == 3) {
+        exit (1); // __VERIFIER_error();
+      }
+      s=5;
+    }
+  }
+  return 42;
+}
