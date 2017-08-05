3bda30226b4f72e3739491c0d41108cce10621c4
caballa <navasjorgea@gmail.com>
2017-08-05 16:58:39 -0700

    new test

---
+++ tests/test-arr-11.c
@@ -0,0 +1,35 @@
+// RUN: %crabllvm -O0 --lower-unsigned-icmp --crab-dom=int --crab-track=arr --crab-check=assert "%s" 2>&1 | OutputCheck %s
+// CHECK: ^1  Number of total safe checks$
+// CHECK: ^0  Number of total error checks$
+// CHECK: ^0  Number of total warning checks$
+extern int nd (void);
+extern void __CRAB_assert(int);
+
+typedef struct {
+  int x;
+  int y;  
+} gstate;
+
+gstate g;
+int a[10];
+const char* str = "hello world";
+
+int main ()
+{
+  g.x = 5;
+  g.y = 3;
+  int i;
+  int *p =&g.x;
+  *p= *p + 1;
+  for (i=0;i<10;i++)
+  {
+    if (nd ())
+      a[i] =g.y;
+    else 
+      a[i] =g.x;
+  }
+  g.y++;
+  int res = a[i-1];
+  __CRAB_assert(res >= 0 && res <= 6);
+  return res + str[0];
+}
