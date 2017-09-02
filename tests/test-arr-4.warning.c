9859e51db93280a211e96afcdfc5344c4721e650
caballa <navasjorgea@gmail.com>
2017-09-02 15:52:52 -0400

    Added test

---
+++ tests/test-arr-4.warning.c
@@ -0,0 +1,37 @@
+// RUN: %crabllvm -O0 --lower-unsigned-icmp --crab-inter --crab-dom=int --crab-track=arr --crab-check=assert "%s" 2>&1 | OutputCheck %s
+// CHECK: ^0  Number of total error checks$
+// CHECK: ^1  Number of total warning checks$
+extern int nd ();
+extern void __CRAB_assert(int);
+
+int x = 5;
+int y = 3;
+
+void foo ()
+{
+  x++;
+}
+
+void bar ()
+{
+  y++;
+}
+
+int a[10];
+
+int main ()
+{
+  int i;
+  foo ();
+  for (i=0;i<10;i++)
+  {
+    if (nd ())
+      a[i] =y;
+    else 
+      a[i] =x;
+  }
+  bar ();
+  int res = a[i-1];
+  __CRAB_assert(res >= 6);
+  return res;
+}
