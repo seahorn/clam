0766e2ccda72e2d238f3c4399c64751a8c977080
caballa <navasjorgea@gmail.com>
2017-10-26 15:57:22 -0700

    Add new tests

---
+++ tests/simple/test-loop-1.c
@@ -0,0 +1,26 @@
+// RUN: %crabllvm -O0 --inline --lower-unsigned-icmp --crab-dom=int --crab-check=assert "%s" 2>&1 | OutputCheck -l debug %s
+// CHECK: ^1  Number of total safe checks$
+// CHECK: ^0  Number of total error checks$
+// CHECK: ^0  Number of total warning checks$
+
+extern void __VERIFIER_error() __attribute__ ((__noreturn__));
+void __VERIFIER_assert(int cond) {
+  if (!(cond)) {
+    ERROR: __VERIFIER_error();
+  }
+  return;
+}
+
+/** 
+   The fact x is unsigned makes this program hard to verify because it
+   requires non-trivial boolean reasoning due to the lowering of
+   unsigned < operator and >= to signed operators.
+**/
+
+int main(void) {
+  unsigned int x = 0;
+  while (x < 0x0fffffff) {
+    x++;
+  }
+  __VERIFIER_assert(x >= 0x0fffffff);
+}
