36c74125fadf0c57ab44933fbe053a54af64bcff
caballa <navasjorgea@gmail.com>
2017-12-18 17:22:59 -0800

    Add a test for the wrapped interval domain

---
+++ tests/simple/test-wrapped.c
@@ -0,0 +1,21 @@
+// RUN: %crabllvm -O0 --crab-dom=w-int --crab-check=assert "%s" 2>&1 | OutputCheck %s
+// CHECK: ^1  Number of total safe checks$
+// CHECK: ^0  Number of total error checks$
+// CHECK: ^0  Number of total warning checks$
+
+extern int nd(void);
+extern void process(char);
+extern void __CRAB_assert(int);
+
+int main() {
+  char x,y;
+  
+  y=-10;
+  if(nd()) x=0;
+  else  x=100;
+  while (x >= y){   
+    x = x-y;        
+  }                 
+  __CRAB_assert(x >= -128 && x <= -119); 
+  return 0;
+}
