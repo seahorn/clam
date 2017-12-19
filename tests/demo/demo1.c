e9eb1f9c734c669288252322ddfb298639a8ecbd
caballa <navasjorgea@gmail.com>
2017-12-18 22:10:21 -0800

    Travis runs README.md examples

---
+++ tests/demo/demo1.c
@@ -0,0 +1,26 @@
+// RUN: %crabllvm -O0 --crab-dom=zones  --crab-check=assert "%s" 2>&1 | OutputCheck -l debug %s
+// CHECK: ^2  Number of total safe checks$
+// CHECK: ^0  Number of total error checks$
+// CHECK: ^0  Number of total warning checks$
+
+
+extern void __CRAB_assume (int);
+extern void __CRAB_assert(int);
+extern int  __CRAB_nd(void);
+
+int main() {
+  int k = __CRAB_nd();
+  int n = __CRAB_nd();
+  __CRAB_assume (k > 0);
+  __CRAB_assume (n > 0);
+  
+  int x = k;
+  int y = k;
+  while (x < n) {
+    x++;
+    y++;
+  }
+  __CRAB_assert (x >= y);
+  __CRAB_assert (x <= y);  
+  return 0;
+}
