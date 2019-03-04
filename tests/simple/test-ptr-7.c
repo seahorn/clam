e8fd912f2b8f3dda78bd0ed27f5ee3979b522325
caballa <navasjorgea@gmail.com>
2019-03-03 18:31:58 -0800

    Added more tests

---
+++ tests/simple/test-ptr-7.c
@@ -0,0 +1,24 @@
+extern short nd_short ();
+extern int nd_int ();
+
+int a[40];
+
+/* 
+   Larger array unified with smaller array.
+   The a and p are unified and collapsed.
+ */
+
+int main() {
+  int i;
+
+  for(i=0;i<10;++i) { 
+    a[i] = 7;
+  }
+
+  short* p = (short*) &a[0];
+  for(i=0;i<20;++i) {
+    p[i] = 8; 
+  } 
+  
+  return p[1];   
+}
