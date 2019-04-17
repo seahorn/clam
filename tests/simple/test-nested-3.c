a7ace7bd111e6460cdfd7e57fb4dc713de19d273
caballa <navasjorgea@gmail.com>
2019-04-17 16:15:23 -0700

    Added tests from Amato and Scozzari paper

---
+++ tests/simple/test-nested-3.c
@@ -0,0 +1,25 @@
+// RUN: %crabllvm -O0 --crab-dom=zones --crab-check=assert --crab-sanity-checks "%s" 2>&1 | OutputCheck -l debug %s
+// CHECK: ^1  Number of total safe checks$
+// CHECK: ^0  Number of total error checks$
+// CHECK: ^0  Number of total warning checks$
+
+extern void __CRAB_assert(int);
+
+// program hh from Scozzari SAS'13
+int main(void) {
+  int i,j;
+  
+  i=0;
+  while (i<4) {
+    j=0;
+    while (j < 4) {
+      __CRAB_assert(i <= j+3);
+      i++;
+      j++;
+    }
+    i = i-j+1;
+  }
+  
+  return 0;
+}
+
