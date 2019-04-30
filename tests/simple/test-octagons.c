dde19673bbf49bed8802ee5296c50cd01b2f097d
caballa <navasjorgea@gmail.com>
2019-04-30 10:44:15 -0700

    Enable test again

--- tests/simple/test-octagons.c
+++ tests/simple/test-octagons.c
@@ -2,7 +2,6 @@
 // CHECK: ^1  Number of total safe checks$
 // CHECK: ^0  Number of total error checks$
 // CHECK: ^0  Number of total warning checks$
-// XFAIL: *
 extern void __CRAB_assert(int);
 extern void __SEAHORN_error(int);
 
