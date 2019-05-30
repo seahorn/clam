faabf5ed16003e1146d14948d2c1d2d76549d340
caballa <navasjorgea@gmail.com>
2019-05-30 15:54:01 -0700

    Recognize infinite loop pattern to identify exit block

--- lib/CrabLlvm/CfgBuilder.cc
+++ lib/CrabLlvm/CfgBuilder.cc
@@ -3011,17 +3011,19 @@ namespace crab_llvm {
       
       // -- process the exit block of the function and its returned value.
       if (ReturnInst *RI = dyn_cast<ReturnInst>(B.getTerminator())) {
-	if (ret_block)
-	  CRABLLVM_ERROR("UnifyFunctionExitNodes pass should be run first",__FILE__, __LINE__);
+	if (ret_block) {
+	  //UnifyFunctionExitNodes ensures *at most* one return
+	  //instruction per function.
+	  CRABLLVM_ERROR("UnifyFunctionExitNodes pass should be run first",
+			 __FILE__, __LINE__);
+	}
 	
         basic_block_t &bb = *BB;
         ret_block = &bb;
 	m_cfg->set_exit(ret_block->label());
-
 	if (has_seahorn_fail) {
 	  ret_block->assertion(lin_cst_t::get_false(), getDebugLoc(RI));	  
 	}
-	
 	if (m_is_inter_proc) {
 	  if (Value * RV = RI->getReturnValue()) {
 	    if (isTracked(*RV, m_lfac.get_track())) {
@@ -3030,7 +3032,6 @@ namespace crab_llvm {
 	    }
 	  }
 	}
-	
       } else {
 	std::vector<const BasicBlock*> succs_vector(succs(B).begin(), succs(B).end());
 	// The default destination of a switch instruction does not
@@ -3038,7 +3039,6 @@ namespace crab_llvm {
 	if (SwitchInst* SI = dyn_cast<SwitchInst>(B.getTerminator())) {
 	  succs_vector.push_back(SI->getDefaultDest());
 	}
-	
         for (const BasicBlock *dst : succs_vector) {
           // -- move branch condition in bb to a new block inserted
           //    between bb and dst
@@ -3238,7 +3238,6 @@ namespace crab_llvm {
     }
 
     if (m_cfg->has_exit()) {
-      #if 1
       // -- Connect all sink blocks with an unreachable instruction to
       //    the exit block.  For a forward analysis this doesn't have
       //    any impact since unreachable becomes bottom anyway.
@@ -3261,18 +3260,53 @@ namespace crab_llvm {
 	  }
 	}
       }
-      #endif 
     } else {
-      // If there is no exit block so far, we search for the first
-      // block without successors.
-      // XXX: we won't have an exit block with programs like this:
+      // We did not find an exit block yet:
+
+      // (1) search for this pattern:
       //   entry: goto loop;
       //    loop: goto loop;
-      for (auto &B: m_func) {
-	if (opt_basic_block_t b = lookup(B)) {
-	  auto it_pair = (*b).next_blocks();
-	  if (it_pair.first == it_pair.second) {
-	    m_cfg->set_exit((*b).label());
+      BasicBlock& entry = m_func.getEntryBlock();
+      auto entry_next = succs(entry);
+      if (std::distance(entry_next.begin(), entry_next.end()) == 1) {
+	const BasicBlock* succ = *(entry_next.begin());
+	auto succ_next = succs(*succ);
+	if (std::distance(succ_next.begin(), succ_next.end()) == 1) {
+	  if ((*(succ_next.begin())) == succ) {
+	    if (opt_basic_block_t exit = lookup(*succ)) {
+	      m_cfg->set_exit((*exit).label());
+	    }
+	  }
+	}
+      }
+
+      if (!m_cfg->has_exit()) {
+	// (2) We check if there is a block with an unreachable
+	// instruction. The pass UnifyFunctionExitNodes ensures that
+	// there is at most one unreachable instruction.
+	for (auto &B: m_func) {
+	  for (auto &I: B) {
+	    if (isa<UnreachableInst>(I)) {
+	      if (opt_basic_block_t b = lookup(B)) {
+		m_cfg->set_exit((*b).label());
+		break;
+	      }
+	    }
+	  }
+	  if (m_cfg->has_exit()) {
+	    break;
+	  }
+	}
+      }
+
+      if (!m_cfg->has_exit()) {
+	// (3) Search for the first block without successors.
+	for (auto &B: m_func) {
+	  if (opt_basic_block_t b = lookup(B)) {
+	    auto it_pair = (*b).next_blocks();
+	    if (it_pair.first == it_pair.second) {
+	      m_cfg->set_exit((*b).label());
+	    }
 	  }
 	}
       }
