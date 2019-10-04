e20ce1168eb8b0b39ff4c1bbfc04877010d365a3
caballa <navasjorgea@gmail.com>
2019-10-04 16:55:25 -0700

    Run soct domain on the test

--- tests/simple/test-octagons.c
+++ tests/simple/test-octagons.c
@@ -1,4 +1,5 @@
 // RUN: %crabllvm -O0 --crab-dom=oct --crab-check=assert --crab-sanity-checks "%s" 2>&1 | OutputCheck %s
+// RUN: %crabllvm -O0 --crab-dom=soct --crab-check=assert --crab-sanity-checks "%s" 2>&1 | OutputCheck %s
 // CHECK: ^1  Number of total safe checks$
 // CHECK: ^0  Number of total error checks$
 // CHECK: ^0  Number of total warning checks$
