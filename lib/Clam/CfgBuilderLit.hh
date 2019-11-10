dbf29affcf913ecd16a11a9a0227564e1472d784
caballa <navasjorgea@gmail.com>
2019-11-10 13:55:39 -0800

    clang-format

--- lib/Clam/CfgBuilderLit.hh
+++ lib/Clam/CfgBuilderLit.hh
@@ -5,9 +5,9 @@
 #include "llvm/IR/Instructions.h"
 #include "llvm/Support/raw_ostream.h"
 
+#include "CfgBuilderMemReg.hh"
 #include "clam/CfgBuilderParams.hh"
 #include "clam/crab/crab_cfg.hh"
-#include "CfgBuilderMemReg.hh"
 
 #include <unordered_map>
 
@@ -21,68 +21,65 @@ public:
     CRAB_LITERAL_INT,
     CRAB_LITERAL_PTR,
   };
-  
+
   crabLit(lit_class_t lit_class) : m_lit_class(lit_class) {}
-  
+
   virtual ~crabLit() {}
-  
+
   bool isBool() const { return m_lit_class == CRAB_LITERAL_BOOL; }
-  
+
   bool isInt() const { return m_lit_class == CRAB_LITERAL_INT; }
-  
+
   bool isPtr() const { return m_lit_class == CRAB_LITERAL_PTR; }
-  
+
   virtual bool isVar() const = 0;
-  
+
   virtual var_t getVar() const = 0;
 
   virtual void write(crab::crab_os &out) const = 0;
-  
+
 private:
   lit_class_t m_lit_class;
 };
 
-inline crab::crab_os& operator<<(crab::crab_os& out, const crabLit& l){
+inline crab::crab_os &operator<<(crab::crab_os &out, const crabLit &l) {
   l.write(out);
   return out;
 }
-  
-/** A Boolean literal is either a variable or constants true and false. **/   
-class crabBoolLit: public crabLit {
+
+/** A Boolean literal is either a variable or constants true and false. **/
+class crabBoolLit : public crabLit {
   friend class crabLitFactoryImpl;
-  
+
   bool m_cst; // only considered if m_var.is_null()
   var_ref_t m_var;
-  
-  crabBoolLit(bool cst): crabLit(CRAB_LITERAL_BOOL), m_cst(cst) {}
-  
-  crabBoolLit(var_t var): crabLit(CRAB_LITERAL_BOOL), m_var(var) {}
-  
+
+  crabBoolLit(bool cst) : crabLit(CRAB_LITERAL_BOOL), m_cst(cst) {}
+
+  crabBoolLit(var_t var) : crabLit(CRAB_LITERAL_BOOL), m_var(var) {}
+
 public:
-  
-  bool isVar() const override {
-    return (!m_var.is_null());
-  }
-  
+  bool isVar() const override { return (!m_var.is_null()); }
+
   var_t getVar() const override {
-    assert (isVar());
+    assert(isVar());
     return m_var.get();
   }
-  
-  bool isConst() const {
-    return (m_var.is_null());
-  }
-  
+
+  bool isConst() const { return (m_var.is_null()); }
+
   bool isTrue() const {
-    if (!isConst()) return false;
+    if (!isConst())
+      return false;
     return m_cst;
   }
-  
+
   bool isFalse() const {
-    if (!isConst()) return false;
-    return !m_cst; 
+    if (!isConst())
+      return false;
+    return !m_cst;
   }
-  
+
   void write(crab::crab_os &out) const override {
     if (isVar()) {
       out << getVar();
@@ -93,29 +90,26 @@ public:
     }
   }
 };
-  
+
 /** A pointer literal is either a variable or constant null.**/
-class crabPtrLit: public crabLit {
+class crabPtrLit : public crabLit {
   friend class crabLitFactoryImpl;
-  
+
   var_ref_t m_lit; // if m_lit.is_null() then the literal represents null
-  
-  crabPtrLit()
-    : crabLit(CRAB_LITERAL_PTR) {} // null
-  crabPtrLit(var_t v)
-    : crabLit(CRAB_LITERAL_PTR), m_lit(v) {}
-  
+
+  crabPtrLit() : crabLit(CRAB_LITERAL_PTR) {} // null
+  crabPtrLit(var_t v) : crabLit(CRAB_LITERAL_PTR), m_lit(v) {}
+
 public:
-  
   bool isVar() const override { return !m_lit.is_null(); }
-    
+
   var_t getVar() const override {
-    assert (isVar());
+    assert(isVar());
     return m_lit.get();
   }
-    
+
   bool isNull() const { return m_lit.is_null(); }
-  
+
   void write(crab::crab_os &out) const override {
     if (isVar()) {
       out << getVar();
@@ -124,36 +118,35 @@ public:
     }
   }
 };
-  
+
 /** A numerical literal is either a variable or constant number.**/
-class crabIntLit: public crabLit {
+class crabIntLit : public crabLit {
   friend class crabLitFactoryImpl;
-  
+
   number_t m_num; // only considered if m_var.is_null();
   var_ref_t m_var;
-  
+
   // If z_number != number_t we assume that number_t has a
   // constructor for z_number.
-  explicit crabIntLit(ikos::z_number n): crabLit(CRAB_LITERAL_INT), m_num(n) {}
-  
-  explicit crabIntLit(var_t v): crabLit(CRAB_LITERAL_INT), m_var(v) {}
-  
+  explicit crabIntLit(ikos::z_number n) : crabLit(CRAB_LITERAL_INT), m_num(n) {}
+
+  explicit crabIntLit(var_t v) : crabLit(CRAB_LITERAL_INT), m_var(v) {}
+
 public:
-  
   bool isVar() const override { return !m_var.is_null(); }
-  
-  var_t getVar() const override{
-    assert (isVar());
+
+  var_t getVar() const override {
+    assert(isVar());
     return m_var.get();
   }
-  
+
   bool isInt() const { return m_var.is_null(); }
-  
+
   number_t getInt() const {
-    assert (isInt());
+    assert(isInt());
     return m_num;
   }
-  
+
   lin_exp_t getExp() const {
     if (isInt())
       return lin_exp_t(getInt());
@@ -162,7 +155,7 @@ public:
       return lin_exp_t(getVar());
     }
   }
-  
+
   void write(crab::crab_os &out) const override {
     if (isVar()) {
       out << getVar();
@@ -177,119 +170,117 @@ typedef std::shared_ptr<crabLit> crab_lit_ref_t;
 /* Implementation of a factory to create literals */
 class crabLitFactoryImpl {
 public:
-  
-  crabLitFactoryImpl(llvm_variable_factory &vfac, const CrabBuilderParams& params);
-  
-  llvm_variable_factory& get_vfac() { return m_vfac; }
-  
-  const CrabBuilderParams& get_cfg_builder_params() const { return m_params;}
-  
+  crabLitFactoryImpl(llvm_variable_factory &vfac,
+                     const CrabBuilderParams &params);
+
+  llvm_variable_factory &get_vfac() { return m_vfac; }
+
+  const CrabBuilderParams &get_cfg_builder_params() const { return m_params; }
+
   // Translate v into a crab literal based on v's type
   crab_lit_ref_t getLit(const llvm::Value &v);
-  
+
   // Create a fresh integer array variable
   var_t mkIntArrayVar(unsigned bitwidth);
-  
-  // Create a fresh boolean array variable    
+
+  // Create a fresh boolean array variable
   var_t mkBoolArrayVar();
-  
-  // Create a fresh pointer array variable        
-  var_t mkPtrArrayVar();        
-  
+
+  // Create a fresh pointer array variable
+  var_t mkPtrArrayVar();
+
   // Create an array variable associated with region r.
   var_t mkArrayVar(mem_region_t r);
-  
+
   // Create an scalar variable associated with region r.
   var_t mkArraySingletonVar(mem_region_t r);
-  
+
   // Create a fresh integer variable of bitwidth bits
   var_t mkIntVar(unsigned bitwidth);
 
   // Create a fresh boolean variable
-    var_t mkBoolVar();
-  
-    // Create a fresh pointer variable
+  var_t mkBoolVar();
+
+  // Create a fresh pointer variable
   var_t mkPtrVar();
-  
+
   // Create a fresh variable from a Value
-  llvm::Optional<var_t> mkVar(const llvm::Value&v);
-  
+  llvm::Optional<var_t> mkVar(const llvm::Value &v);
+
   // Common accessors to crab_lit_ref_t subclasses.
   bool isBoolTrue(const crab_lit_ref_t ref) const;
-  
+
   bool isBoolFalse(const crab_lit_ref_t ref) const;
-  
+
   bool isPtrNull(const crab_lit_ref_t ref) const;
-  
+
   number_t getIntCst(const crab_lit_ref_t ref) const;
-  
+
   lin_exp_t getExp(const crab_lit_ref_t ref) const;
-        
+
 private:
-  
-  typedef std::unordered_map<const llvm::Value*, crab_lit_ref_t> lit_cache_t;
+  typedef std::unordered_map<const llvm::Value *, crab_lit_ref_t> lit_cache_t;
   typedef typename lit_cache_t::value_type binding_t;
-  
+
   llvm_variable_factory &m_vfac;
   const CrabBuilderParams &m_params;
   lit_cache_t m_lit_cache;
-  
+
   llvm::Optional<crabBoolLit> getBoolLit(const llvm::Value &v);
   llvm::Optional<crabIntLit> getIntLit(const llvm::Value &v);
-  llvm::Optional<crabPtrLit> getPtrLit(const llvm::Value &v);    
+  llvm::Optional<crabPtrLit> getPtrLit(const llvm::Value &v);
 };
 
-/** 
+/**
  *  Factory to create crab literals: typed variable or number.
  **/
 class crabLitFactory {
 public:
-    
-  crabLitFactory(llvm_variable_factory &vfac, const CrabBuilderParams& params);
-  
+  crabLitFactory(llvm_variable_factory &vfac, const CrabBuilderParams &params);
+
   ~crabLitFactory();
-  
-  llvm_variable_factory& get_vfac();
+
+  llvm_variable_factory &get_vfac();
 
   crab::cfg::tracked_precision get_track() const;
-  
-  const CrabBuilderParams& get_cfg_builder_params() const;
-  
+
+  const CrabBuilderParams &get_cfg_builder_params() const;
+
   /** convert a Value to a crabLit **/
   crab_lit_ref_t getLit(const llvm::Value &v);
-  
+
   /** make typed variables **/
   var_t mkIntVar(unsigned bitwidth);
-  
+
   var_t mkBoolVar();
-  
+
   var_t mkPtrVar();
-  
-  llvm::Optional<var_t> mkVar(const llvm::Value&v);
-  
+
+  llvm::Optional<var_t> mkVar(const llvm::Value &v);
+
   var_t mkIntArrayVar(unsigned bitwidth);
-  
+
   var_t mkBoolArrayVar();
-  
+
   var_t mkPtrArrayVar();
-  
-  template<typename Region> var_t mkArrayVar(Region r);
-  
-  template<typename Region> var_t mkArraySingletonVar(Region r);    
-  
+
+  template <typename Region> var_t mkArrayVar(Region r);
+
+  template <typename Region> var_t mkArraySingletonVar(Region r);
+
   /** direct accessors to crabLit subclasses **/
   bool isBoolTrue(const crab_lit_ref_t ref) const;
-  
+
   bool isBoolFalse(const crab_lit_ref_t ref) const;
-  
+
   bool isPtrNull(const crab_lit_ref_t ref) const;
-  
+
   lin_exp_t getExp(const crab_lit_ref_t ref) const;
-  
-  number_t getIntCst(const crab_lit_ref_t ref) const;    
-  
+
+  number_t getIntCst(const crab_lit_ref_t ref) const;
+
 private:
-  crabLitFactoryImpl* m_impl;
+  crabLitFactoryImpl *m_impl;
 };
-  
+
 } // end namespace clam
--- lib/Clam/CfgBuilderMemReg.hh
+++ lib/Clam/CfgBuilderMemReg.hh
@@ -2,15 +2,15 @@
 
 #include "clam/HeapAbstraction.hh"
 
-/* 
+/*
    Helpers for memory regions.
-   
+
    We don't add array statements for memory regions containing
    pointers. This means that if the load's lhs or store value
    operand is a pointer we only add the corresponding pointer
    statement (ptr_load/ptr_store) but not any extra array statement
-   (array_load/array_store). 
-   
+   (array_load/array_store).
+
    FIXME: If we would want to add array statements with elements of
    pointer type, we need to do some renaming. Otherwise, for
    instance, for the lhs of a load instruction, the same variable
@@ -20,11 +20,11 @@
 
 namespace clam {
 
-typedef typename HeapAbstraction::region_t mem_region_t;  
+typedef typename HeapAbstraction::region_t mem_region_t;
 typedef typename HeapAbstraction::region_vector_t mem_region_vector_t;
-  
-inline mem_region_t
-get_region(HeapAbstraction &mem, llvm::Function &f, llvm::Value *v) {
+
+inline mem_region_t get_region(HeapAbstraction &mem, llvm::Function &f,
+                               llvm::Value *v) {
   mem_region_t res = mem.getRegion(f, v);
   if (res.get_type() == INT_REGION || res.get_type() == BOOL_REGION) {
     return res;
@@ -34,50 +34,54 @@ get_region(HeapAbstraction &mem, llvm::Function &f, llvm::Value *v) {
 }
 
 // Return whether the region contains a singleton alias class
-template<typename HeapAbstraction>
-inline const llvm::Value* get_singleton_value(Region<HeapAbstraction> r,
-					      bool enable_unique_scalars) {
+template <typename HeapAbstraction>
+inline const llvm::Value *get_singleton_value(Region<HeapAbstraction> r,
+                                              bool enable_unique_scalars) {
   if (enable_unique_scalars) {
-    if (r.isUnknown()) return nullptr;
+    if (r.isUnknown())
+      return nullptr;
     if (r.get_type() == INT_REGION || r.get_type() == BOOL_REGION) {
-      if (const llvm::Value* v = r.getSingleton()) {
-	return v;
+      if (const llvm::Value *v = r.getSingleton()) {
+        return v;
       }
     }
   }
   return nullptr;
 }
-  
-template<typename V>
-inline mem_region_vector_t get_read_only_regions(HeapAbstraction &mem, V& v) {
+
+template <typename V>
+inline mem_region_vector_t get_read_only_regions(HeapAbstraction &mem, V &v) {
   mem_region_vector_t res;
   auto regions = mem.getOnlyReadRegions(v);
   std::copy_if(regions.begin(), regions.end(), std::back_inserter(res),
-	       [](mem_region_t r){
-		 return r.get_type() == INT_REGION || r.get_type() == BOOL_REGION;
-	       });
+               [](mem_region_t r) {
+                 return r.get_type() == INT_REGION ||
+                        r.get_type() == BOOL_REGION;
+               });
   return res;
 }
 
-template<typename V>
-inline mem_region_vector_t get_modified_regions(HeapAbstraction &mem, V& v) {
+template <typename V>
+inline mem_region_vector_t get_modified_regions(HeapAbstraction &mem, V &v) {
   mem_region_vector_t res;
   auto regions = mem.getModifiedRegions(v);
   std::copy_if(regions.begin(), regions.end(), std::back_inserter(res),
-	       [](mem_region_t r){
-		 return r.get_type() == INT_REGION || r.get_type() == BOOL_REGION;
-	       });
+               [](mem_region_t r) {
+                 return r.get_type() == INT_REGION ||
+                        r.get_type() == BOOL_REGION;
+               });
   return res;
 }
-  
-template<typename V>
-inline mem_region_vector_t get_new_regions(HeapAbstraction &mem, V& v) {
+
+template <typename V>
+inline mem_region_vector_t get_new_regions(HeapAbstraction &mem, V &v) {
   mem_region_vector_t res;
   auto regions = mem.getNewRegions(v);
   std::copy_if(regions.begin(), regions.end(), std::back_inserter(res),
-	       [](mem_region_t r){
-		 return r.get_type() == INT_REGION || r.get_type() == BOOL_REGION;
-	       });
+               [](mem_region_t r) {
+                 return r.get_type() == INT_REGION ||
+                        r.get_type() == BOOL_REGION;
+               });
   return res;
 }
 
