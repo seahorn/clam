473101d11bbc5d0edf95640d37aaf286b6b18c5a
caballa <navasjorgea@gmail.com>
2020-09-15 16:23:00 -0500

    test: added new test for region domain

---
+++ tests/mem/test-mem-8.c
@@ -0,0 +1,48 @@
+/* c/loops/veris.c_NetBSD-libc__loop_true-unreach-call_true-termination.i */
+
+// RUN: %clam -O0 --lower-select --crab-inter --crab-dom=zones --crab-track=mem --crab-heap-analysis=cs-sea-dsa --crab-check=assert --crab-sanity-checks "%s" 2>&1 | OutputCheck %s
+// CHECK: ^1  Number of total safe checks$
+// CHECK: ^0  Number of total warning checks$
+// XFAIL: *
+
+/* TODO: need to implement some corner cases in the translation to
+   Crab */
+
+extern void __VERIFIER_error() __attribute__ ((__noreturn__));
+
+void __VERIFIER_assert(int cond) {
+  if (!(cond)) {
+    ERROR: __VERIFIER_error();
+  }
+  return;
+}
+typedef int Char;
+
+
+Char *tmp;
+
+int glob2 (Char *pathbuf, Char *pathlim)
+{
+  Char *p;
+
+  for (p = pathbuf; p <= pathlim; p++) {
+
+    __VERIFIER_assert(p<=tmp);
+    *p = 1;
+  }
+
+  return 0;
+}
+
+int main ()
+{
+  Char pathbuf[1 +1];
+
+  Char *bound = pathbuf + sizeof(pathbuf)/sizeof(*pathbuf) - 1;
+
+  tmp = pathbuf + sizeof(pathbuf)/sizeof(*pathbuf) - 1;
+
+  glob2 (pathbuf, bound);
+
+  return 0;
+}
