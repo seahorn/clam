2251219a527933360efb152865db046f22467555
caballa <navasjorgea@gmail.com>
2020-11-20 11:54:53 -0600

    test: add test for recursive functions

---
+++ tests/mem/test-mem-11.c
@@ -0,0 +1,31 @@
+// RUN: %clam -O0 --crab-inter --crab-inter-recursive-functions --crab-dom=int --crab-track=mem --crab-check=assert --crab-sanity-checks "%s" 2>&1 | OutputCheck %s
+// CHECK: ^1  Number of total safe checks$
+
+extern int int_nd(void);
+extern void __CRAB_assert(int);
+extern void __CRAB_assume(int);
+
+
+/* Initialize an array */
+void rec_init(int* a, int n) {
+  if (n == 0) {
+    a[0] = 5;
+    return;
+  }
+  else {
+    a[n] = 5;
+    rec_init(a, n-1);
+  }
+}
+
+int main () {
+  int a[10];
+  rec_init(&a[0], 9);
+
+  int i = int_nd();
+  __CRAB_assume(i >=0);
+  __CRAB_assume(i < 10);
+  int res = a[i-1];
+  __CRAB_assert(res == 5);
+  return res;
+}
