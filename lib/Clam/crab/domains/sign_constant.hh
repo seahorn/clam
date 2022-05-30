195313e904ffc4cb09570e620c36dbc039c47f13
caballa <navasjorgea@gmail.com>
2022-05-30 11:21:52 -0600

    fix: revert part of last commit.
    I committed unintentionally some changes.

--- lib/Clam/crab/domains/sign_constant.hh
+++ lib/Clam/crab/domains/sign_constant.hh
@@ -7,5 +7,5 @@ namespace clam {
 using BASE(sign_constant_domain_t) =
   crab::domains::sign_constant_domain<number_t, region_subdom_varname_t>;
 using sign_constant_domain_t =
-  RGN_FUN(ARRAY_FUN(BOOL_NUM(VALUE_PARTITION(BASE(sign_constant_domain_t)))));
+  RGN_FUN(ARRAY_FUN(BOOL_NUM(BASE(sign_constant_domain_t))));
 } // end namespace clam
